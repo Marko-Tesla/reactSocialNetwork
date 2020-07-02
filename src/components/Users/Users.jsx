e8b61432da9e71ab34b1a2d2340fa6755203cafc
Marko-tesla <prokudinmarat@gmail.com>
2020-07-03 00:18:33 +0600

    connectedAPI

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,45 +1,71 @@
 import React from 'react';
 import styles from './users.module.css'
+import * as axios from 'axios';
+import userPhoto from '../../assets/avatar-default.png';
 
-let Users = (props) => {
+class Users extends React.Component {
 
-   if (props.users.length === 0 ) {
-      props.setUsers([    
-         {id: 1, photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/8/88/Dmitry_Nagiev_2017_4.jpg' ,followed: true, fullName: 'Marat', status: 'I\'m a boss', location: {city:'Almaty', country: 'Kazakhstan'} },
-         {id: 2, photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/8/88/Dmitry_Nagiev_2017_4.jpg' , followed: true, fullName: 'Orseniy', status: 'I\'m a boss', location: {city:'Chicago', country: 'USA'} },
-         {id: 3, photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/8/88/Dmitry_Nagiev_2017_4.jpg' , followed: false, fullName: 'DanbIla', status: 'I\'m a boss', location: {city:'Almaty', country: 'Kazakhstan'} },
-         {id: 4, photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/8/88/Dmitry_Nagiev_2017_4.jpg' , followed: false, fullName: 'Danik', status: 'I\'m a boss', location: {city:'Sofia', country: 'Bulgary'} }
-      ])
+   componentDidMount() {
+      if (this.props.users.length === 0 ) {
+         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
+         });
+      }   
    }
-   
 
-   return <div>
+   onPageChanged = (pageNumber) => {
+      this.props.setCurrentPage(pageNumber);
+      axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+      .then(response => {
+         this.props.setUsers(response.data.items);
+      });
+   }
+
+   render() {
+
+      let pagesCount = Math.ceil(this.props.totalUsersCount/this.props.pageSize);
+
+      let pages = [];
+      for (let i=1; i<= pagesCount; i++) {
+         pages.push(i);
+      }
+      
+      return <div>
+         <div>
+            {pages.map(p => {
+               return <span className={this.props.currentPage === p && styles.selectedPage}
+               onClick={(e)=>{this.onPageChanged(p)}}>{p}</span>
+            })}
+         </div>
       {
-         props.users.map(u => <div key={u.id}>
+         this.props.users.map(u => <div key={u.id}>
             <span>
                <div>
-                  <img src={u.photoUrl} className={styles.userPhoto} alt='avatar'></img>
+                  <img src={ u.photos.small != null ? u.photos.small: userPhoto} className={styles.userPhoto} alt='avatar'></img>
                </div>
                <div>
                   {u.followed 
-                  ?<button onClick={() => {props.unfollow(u.id)}}>Unfollow</button> 
-                  :<button onClick={() => {props.follow(u.id)}}>Follow</button>}
+                  ?<button onClick={() => {this.props.unfollow(u.id)}}>Unfollow</button> 
+                  :<button onClick={() => {this.props.follow(u.id)}}>Follow</button>}
                   
                </div>
             </span>
             <span>
                <span>
-                  <div>{u.fullName}</div>
+                  <div>{u.name}</div>
                   <div>{u.status}</div>
                </span>
                <span>
-                  <div>{u.location.country}</div>
-                  <div>{u.location.city}</div>
+                  <div>{"u.location.country"}</div>
+                  <div>{"u.location.city"}</div>
                </span>
             </span>
          </div>)
       }
    </div>
+   }
+   
 }
 
 export default Users;
