17103da3dd07ec768ddd54c26b308fef94b01c4e
Marko-tesla <prokudinmarat@gmail.com>
2020-07-11 14:43:39 +0600

    PreloaderCreated

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,9 +1,55 @@
 import React from 'react';
+import styles from './users.module.css';
+import userPhoto from '../../assets/avatar-default.png';
+
+let Users = (props) => {
+   let pagesCount = Math.ceil(props.totalUsersCount/props.pageSize);
+
+      let pages = [];
+      for (let i=1; i<= pagesCount; i++) {
+         pages.push(i);
+      }
+   return <div>
+      <div>
+         {pages.map(p => {
+            return <span className={props.currentPage === p && styles.selectedPage}
+            onClick={(e)=>{props.onPageChanged(p)}}>{p}</span>
+         })}
+      </div>
+      {props.users.map(u => <div key={u.id}>
+         <span>
+            <div>
+               <img src={ u.photos.small != null ? u.photos.small: userPhoto} className={styles.userPhoto} alt='avatar'></img>
+            </div>
+            <div>
+               {u.followed 
+               ?<button onClick={() => {props.unfollow(u.id)}}>Unfollow</button> 
+               :<button onClick={() => {props.follow(u.id)}}>Follow</button>}
+               
+            </div>
+         </span>
+         <span>
+            <span>
+               <div>{u.name}</div>
+               <div>{u.status}</div>
+            </span>
+            <span>
+               <div>{"u.location.country"}</div>
+               <div>{"u.location.city"}</div>
+            </span>
+         </span>
+      </div>)}
+   </div>
+}
+
+export default Users;
+
+/*
 import styles from './users.module.css'
 import * as axios from 'axios';
 import userPhoto from '../../assets/avatar-default.png';
 
-class Users extends React.Component {
+class UsersAPIComponent extends React.Component {
 
    componentDidMount() {
       if (this.props.users.length === 0 ) {
@@ -31,7 +77,8 @@ class Users extends React.Component {
          pages.push(i);
       }
       
-      return <div>
+      return <Users />
+      <div>
          <div>
             {pages.map(p => {
                return <span className={this.props.currentPage === p && styles.selectedPage}
@@ -68,4 +115,5 @@ class Users extends React.Component {
    
 }
 
-export default Users;
+export default UsersAPIComponent;
+*/
