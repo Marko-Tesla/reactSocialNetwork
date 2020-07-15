55297a71266473d5df4f41100e48f0084f7388ea
Marko-tesla <prokudinmarat@gmail.com>
2020-07-15 21:22:15 +0600

    addedReduxThunksForAllQueries

--- src/api/api.js
+++ src/api/api.js
@@ -15,9 +15,18 @@ export const usersAPI = {
       .then(response =>response.data)
    },
    deleteUser(userId) {
-      return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
+      return instance.delete(`follow/${userId}`)
    },
    followUser(userId) {
-      return instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`)
+      return instance.post(`follow/${userId}`)
+   },
+   getProfile(userId) {
+      return instance.get(`profile/`+userId)
+   }
+}
+
+export const authAPI = {
+   me() {
+      return instance.get(`auth/me`)
    }
 }
